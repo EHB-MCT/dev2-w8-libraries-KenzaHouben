ad81c16e237d4d32558712b1bb8515b6410c4fb7
Kenza Houben <kenza.tabti@student.ehb.be>
2025-04-24 11:09:12 +0200

    probleem, coordinaten zitten in de zee

---
+++ js/WC_Brussel.js
@@ -0,0 +1,19 @@
+export default class WC_Brussel {
+    constructor(lon, lat, location) {
+        this._lon = lon;
+        this._lat = lat;
+        this.location = location;
+    }
+
+    get lon() {
+        return this._lon;
+    }
+
+    get lat() {
+        return this._lat;
+    }
+
+    get htmlString() {
+        return `<div>${this._location}</div>`
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-let map; // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
+import WC_Brussel from "./WC_Brussel.js";
+
+// een lege lijst, want we gaan telkens loopen
+const items = [];
+
+let map = L.map('map').setView([50.84294259584643, 4.324465582871228], 13); // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
+
+// let popup = L.popup()
+//     .setLatLng([50.84345464295743, 4.325409130456444])
+//     .setContent("I am a standalone popup.")
+//     .openOn(map);
 
 function init() {
     // initialise de kaart
-    var map = L.map('map').setView([51.505, -0.09], 13);
     // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
     // vergeet openstreetmap attributie niet
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,17 +19,41 @@ function init() {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
     // gebruik de functie "loadMarkers" om de markers toe te voegen
+    loadMarkers();
 }
 
 function loadMarkers() {
+    const URL = 'https://opendata.bruxelles.be/api/explore/v2.1/catalog/datasets/toilettes_publiques_vbx/records?limit=20';
     // fetch de data van opendata.brussels.be
-    // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
-
+    fetch(URL)
+        .then(function (response) {
+            console.log(response);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+            data.results.forEach(function (wc) {
+                console.log(wc);
+                const WC = new WC_Brussel(wc.geo_point_2d.lat, wc.geo_point_2d.lon);
+                console.log(wc.geo_point_2d.lat, wc.geo_point_2d.lon);
+                items.push(WC);
+                // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
+                addMarker(WC.lat, WC.lon);
+            })
+        })
+        .catch(function (error) {
+            console.log('Error fetching data:', error);
+        })
 }
+
 
 function addMarker(lat, lon) {
     // voeg een marker toe op lat, lon
-
+    let marker = L.marker([lat, lon]).addTo(map);
+    // Popups for markers
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 }
 
 init();
