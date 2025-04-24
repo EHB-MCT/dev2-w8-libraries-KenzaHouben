65f7c1f405c209ac1441436026ccd3e999de38a3
Kenza Houben <kenza.tabti@student.ehb.be>
2025-04-24 09:24:36 +0200

    kaart toegevoegd

--- js/index.js
+++ js/index.js
@@ -2,10 +2,13 @@ let map; // gebruik dit om de map gemakkelijk aan te spreken doorheen de applica
 
 function init() {
     // initialise de kaart
-
+    var map = L.map('map').setView([51.505, -0.09], 13);
     // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
     // vergeet openstreetmap attributie niet
-
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
     // gebruik de functie "loadMarkers" om de markers toe te voegen
 }
 
@@ -19,3 +22,5 @@ function addMarker(lat, lon) {
     // voeg een marker toe op lat, lon
 
 }
+
+init();
